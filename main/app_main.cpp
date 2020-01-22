8e4ad98f7e5ce39a0c2c7fa81bf5d85b516e9ae8
Robin van Emden <robin@pwy.nl>
2020-01-22 14:52:53 +0100

    Working example

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -8,39 +8,44 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+#include "driver/gpio.h"
 #include "app_main.h"
+#include "nano_engine.h"
 
 #include "ssd1306.h"
 #include "ssd1306_console.h"
 
 Ssd1306Console console;
 
-template <typename... Args>
-void c_printf(const char *fmt, Args... args)
-{
-    char buffer[80];
-    sprintf(buffer, fmt, args...);
-    printf(buffer);
-    console.print(buffer);
-}
-
 extern "C" void app_main(void)
 {
+
     ssd1306_setFixedFont(ssd1306xled_font6x8);
     
-    // this code works on ssd1306 device:
-
-    // ssd1306_128x64_i2c_init();
-    // ssd1306_flipHorizontal(1);
-    // ssd1306_flipVertical(1);
-    // ssd1306_clearScreen();
-
-
-    // but this throws error on ili9341:
-
+    // ssd1306_128x64_i2c_init(); 
     ili9341_240x320_spi_init(5, 27, 32);
+    
+    gpio_pullup_en(GPIO_NUM_4);
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_4, 1);
 
-    c_printf("Test\n");
+    ssd1306_clearScreen( );
+
+    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_clearScreen();
+    ssd1306_setColor(RGB_COLOR16(255,255,0));
+    ssd1306_printFixed(0,  8, "Normal text", STYLE_NORMAL);
+    ssd1306_setColor(RGB_COLOR16(0,255,0));
+    ssd1306_printFixed(0, 16, "Bold text", STYLE_BOLD);
+    ssd1306_setColor(RGB_COLOR16(0,255,255));
+    ssd1306_printFixed(0, 24, "Italic text", STYLE_ITALIC);
+    ssd1306_negativeMode();
+    ssd1306_setColor(RGB_COLOR16(255,255,255));
+    ssd1306_printFixed(0, 32, "Inverted bold", STYLE_BOLD);
+    ssd1306_positiveMode();
+
+
+    console.print("\nTesting one two\n");
 
     vTaskDelay(30 / portTICK_PERIOD_MS);
 }
